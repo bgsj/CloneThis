d07ae841347e96cfc1dd5ca41c809e853161f03a
bryan.sanjuan <ebgsj@rocketmail.com>
2015-02-03 13:32:25 +0800

    test

--- FirstGitProject/src/Test.java
+++ FirstGitProject/src/Test.java
@@ -3,8 +3,11 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Hi BRYAN");
-		//new comment
-		//another comment.
-        //intel
-	}
+		/*
+		new comment
+		another comment.
+		intel
+		bryan
+		*/
+    }
 }
